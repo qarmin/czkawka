68cbbe8c4562919ef7a4982a5f02aac06d1e70b8
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-08-26 11:17:16 +0200

    First commit

---
+++ src/duplicate_finder.rs
@@ -0,0 +1,25 @@
+// Todo, należy upewnić się, że ma wystarczające uprawnienia do odczytu i usuwania
+
+
+struct DuplicateFinder {
+    number_of_all_files_with_duplicates : u64,
+    number_of_duplicated_files : u64,
+    //map_of_values : u32,
+    excluded_directories : Vec<String>,
+    included_directories : Vec<String>
+}
+
+impl DuplicateFinder{
+
+
+}
+
+
+struct FileEntry {
+    file_path : String,
+    file_size : u64
+}
+
+fn find(){
+
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod duplicate_finder;
+
+fn main() {
+    println!("Hello, world!");
+}
