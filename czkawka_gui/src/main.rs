e22ff1b362e8c5741d6e7485859ad973b63596a1
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-09-04 21:12:18 +0200

    Add basic glade file

--- czkawka_gui/src/main.rs
+++ czkawka_gui/src/main.rs
@@ -6,9 +6,29 @@ use gtk::prelude::*;
 use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-    MessageDialog::new(None::<&Window>, DialogFlags::empty(), MessageType::Info, ButtonsType::Ok, "Hello World").run();
+    gtk::init().expect("Failed to initialize GTK.");
+
+    // Loading glade file content
+    let glade_src = include_str!("../czkawka.glade");
+    // Build UI from glade file
+    let builder = gtk::Builder::from_string(glade_src);
+
+    // Show first window
+    let window : gtk::Window = builder.get_object("window1").unwrap();
+    window.show_all();
+
+
+    let button: gtk::Button = builder.get_object("button1").unwrap();
+    let dialog: gtk::MessageDialog = builder.get_object("messagedialog1").unwrap();
+
+    button.connect_clicked(move |_| {
+        // We make the dialog window blocks all other windows.
+        dialog.run();
+        // When it finished running, we hide it again.
+        dialog.hide();
+    });
+
+// We start the gtk main loop.
+    gtk::main();
+
 }
