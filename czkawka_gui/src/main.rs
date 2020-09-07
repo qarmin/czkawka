b2d1b7b77948ee0b47d4f5355ed7d30e86cf76c3
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-09-07 21:37:18 +0200

    Add basic TreeView

--- czkawka_gui/src/main.rs
+++ czkawka_gui/src/main.rs
@@ -3,7 +3,7 @@ use czkawka_core::{duplicate, empty_folder};
 
 extern crate gtk;
 use gtk::prelude::*;
-use gtk::Builder;
+use gtk::{Builder, TreeView, TreeViewColumn};
 // use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 fn main() {
@@ -41,4 +41,35 @@ fn prepare_buttons_at_start(builder : Builder){
     buttons_delete.hide();
     let buttons_save : gtk::Button = builder.get_object("buttons_save").unwrap();
     buttons_save.hide();
+
+    search_for_duplicates(builder);
+}
+fn search_for_duplicates(builder:Builder){
+
+    let scrolled_window_duplicate_finder : gtk::ScrolledWindow = builder.get_object("scrolled_window_duplicate_finder").unwrap();
+
+
+    let name_column : gtk::TreeViewColumn = TreeViewColumn::new();
+    name_column.set_title("File Name");
+    name_column.set_resizable(true);
+    name_column.set_min_width(50);
+
+    let path_column : gtk::TreeViewColumn = TreeViewColumn::new();
+    path_column.set_title("File Name");
+    path_column.set_resizable(true);
+    path_column.set_min_width(50);
+
+    let col_types: [glib::types::Type; 2] = [
+        glib::types::Type::String,
+        glib::types::Type::String,
+    ];
+    let list_store : gtk::ListStore = gtk::ListStore::new(&col_types);
+
+    let tree_view_duplicate_finder : gtk::TreeView = TreeView::with_model(&list_store);
+
+    tree_view_duplicate_finder.append_column(&name_column);
+    tree_view_duplicate_finder.append_column(&path_column);
+
+    scrolled_window_duplicate_finder.add(&tree_view_duplicate_finder);
+    scrolled_window_duplicate_finder.show_all();
 }
