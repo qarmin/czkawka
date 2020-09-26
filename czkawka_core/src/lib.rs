3cff32c0319b55f01bdb91273036ef3588776d4b
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-09-26 14:13:06 +0200

    Bump to 0.1.3 dev version

--- czkawka_core/src/lib.rs
+++ czkawka_core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod big_file;
-pub mod common;
 pub mod duplicate;
 pub mod empty_folder;
 
-pub const CZKAWKA_VERSION: &str = "0.1.2";
+pub mod common;
+
+pub const CZKAWKA_VERSION: &str = "0.1.3";
