bf05cd53722aed5a348db653f45c93a4e6a93f91
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-09-29 14:37:50 +0200

    Move arguments pair to different file

--- czkawka_cli/src/main.rs
+++ czkawka_cli/src/main.rs
@@ -1,3 +1,6 @@
+mod arguments_pair;
+
+use crate::arguments_pair::ArgumentsPair;
 use czkawka_core::common_traits::*;
 use czkawka_core::*;
 use std::{env, process};
@@ -390,35 +393,4 @@ fn print_help() {
 
     "###
     );
-}
-
-struct ArgumentsPair {
-    command: String,
-    argument: Option<String>,
-}
-
-impl ArgumentsPair {
-    pub fn has_command(ar: &[ArgumentsPair], command: &str) -> bool {
-        for a in ar {
-            if a.command == command {
-                return true;
-            }
-        }
-        false
-    }
-    pub fn get_argument(ar: &[ArgumentsPair], command: &str, can_be_empty: bool) -> String {
-        for a in ar {
-            if a.command == command {
-                if !can_be_empty && a.argument == Option::None {
-                    println!("FATAL ERROR: {} commands should have argument passed", command);
-                    process::exit(1);
-                }
-                return match &a.argument {
-                    Some(t) => t.clone(),
-                    None => "".to_string(),
-                };
-            }
-        }
-        panic!("INTERNAL ERROR: Get argument should always return value");
-    }
 }
