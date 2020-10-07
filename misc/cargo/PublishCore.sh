f00219d6d0ab3182fbc308b39f1021fd000ffa20
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-10-07 09:00:53 +0200

    Added scripts to automatically upgrade cargo packages

---
+++ misc/cargo/PublishCore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+NUMBER="1.0.1"
+CZKAWKA_PATH="/home/rafal"
+
+cd "$CZKAWKA_PATH"
+CZKAWKA_PATH="$CZKAWKA_PATH/czkawka"
+rm -rf $CZKAWKA_PATH
+git clone https://github.com/qarmin/czkawka.git "$CZKAWKA_PATH"
+cd $CZKAWKA_PATH
+git checkout "$NUMBER"
+
+cd "$CZKAWKA_PATH/czkawka_core"
+cargo package
+if [ $(echo $?) != "0"  ]
+then
+  echo "Cargo package failed CORE"
+  exit 1
+fi
+git reset --hard
+
+cd "$CZKAWKA_PATH/czkawka_core"
+cargo publish
+git reset --hard
+
---
+++ misc/cargo/PublishOther.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+NUMBER="1.0.1"
+CZKAWKA_PATH="/home/rafal"
+
+cd "$CZKAWKA_PATH"
+CZKAWKA_PATH="$CZKAWKA_PATH/czkawka"
+rm -rf $CZKAWKA_PATH
+git clone https://github.com/qarmin/czkawka.git "$CZKAWKA_PATH"
+cd $CZKAWKA_PATH
+git checkout "$NUMBER"
+
+
+cd "$CZKAWKA_PATH/czkawka_cli"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_cli/Cargo.toml"
+cargo package --allow-dirty
+if [ $(echo $?) != "0"  ]
+then
+  echo "Cargo package failed CLI"
+  exit 1
+fi
+git reset --hard
+
+
+cd "$CZKAWKA_PATH/czkawka_gui"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_gui/Cargo.toml"
+cargo package --allow-dirty
+if [ $(echo $?) != "0"  ]
+then
+  echo "Cargo package failed GUI"
+  exit 1
+fi
+git reset --hard
+
+cd "$CZKAWKA_PATH/czkawka_gui_orbtk"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_gui_orbtk/Cargo.toml"
+cargo package --allow-dirty
+if [ $(echo $?) != "0"  ]
+then
+  echo "Cargo package failed GUI ORBTK"
+  exit 1
+fi
+git reset --hard
+
+
+
+cd "$CZKAWKA_PATH/czkawka_cli"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_cli/Cargo.toml"
+cargo publish --allow-dirty
+git reset --hard
+
+cd "$CZKAWKA_PATH/czkawka_gui"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_gui/Cargo.toml"
+cargo publish --allow-dirty
+git reset --hard
+
+cd "$CZKAWKA_PATH/czkawka_gui_orbtk"
+sed -i "s/{ path = \"..\/czkawka_core\" }/\"=$NUMBER\"/g" "$CZKAWKA_PATH/czkawka_gui_orbtk/Cargo.toml"
+cargo publish --allow-dirty
+git reset --hard
