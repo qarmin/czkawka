68c3c294c970b15668f2473d7c0a03bedf76fdb8
Rafał Mikrut <41945903+qarmin@users.noreply.github.com>
2020-12-19 10:15:25 +0100

    Add button to enable/disable in settings text view errors (#122)

--- czkawka_gui/src/gui_data.rs
+++ czkawka_gui/src/gui_data.rs
@@ -156,6 +156,7 @@ pub struct GuiData {
     pub check_button_settings_load_at_start: gtk::CheckButton,
     pub check_button_settings_confirm_deletion: gtk::CheckButton,
     pub check_button_settings_show_preview_similar_images: gtk::CheckButton,
+    pub check_button_settings_show_text_view: gtk::CheckButton,
 
     pub button_settings_save_configuration: gtk::Button,
     pub button_settings_load_configuration: gtk::Button,
@@ -365,6 +366,7 @@ impl GuiData {
         let check_button_settings_load_at_start: gtk::CheckButton = builder.get_object("check_button_settings_load_at_start").unwrap();
         let check_button_settings_confirm_deletion: gtk::CheckButton = builder.get_object("check_button_settings_confirm_deletion").unwrap();
         let check_button_settings_show_preview_similar_images: gtk::CheckButton = builder.get_object("check_button_settings_show_preview_similar_images").unwrap();
+        let check_button_settings_show_text_view: gtk::CheckButton = builder.get_object("check_button_settings_show_text_view").unwrap();
 
         let button_settings_save_configuration: gtk::Button = builder.get_object("button_settings_save_configuration").unwrap();
         let button_settings_load_configuration: gtk::Button = builder.get_object("button_settings_load_configuration").unwrap();
@@ -469,6 +471,7 @@ impl GuiData {
             check_button_settings_load_at_start,
             check_button_settings_confirm_deletion,
             check_button_settings_show_preview_similar_images,
+            check_button_settings_show_text_view,
             button_settings_save_configuration,
             button_settings_load_configuration,
             button_settings_reset_configuration,
