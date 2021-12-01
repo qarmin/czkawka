b1fba357551627c9770d6cece8f9ec257f59c7b2
Rafał Mikrut <mikrutrafal@protonmail.com>
2021-12-01 12:36:59 +0100

    Not needing to handle everything

--- czkawka_gui/src/connect_duplicate_buttons.rs
+++ czkawka_gui/src/connect_duplicate_buttons.rs
@@ -18,36 +18,4 @@ pub fn connect_duplicate_buttons(gui_data: &GuiData) {
             radio_button_hash_type_crc32.set_sensitive(false);
         }
     });
-    let radio_button_duplicates_hash = gui_data.main_notebook.radio_button_duplicates_hash.clone();
-    let radio_button_duplicates_name = gui_data.main_notebook.radio_button_duplicates_name.clone();
-    let radio_button_hash_type_blake3 = gui_data.main_notebook.radio_button_hash_type_blake3.clone();
-    let radio_button_hash_type_xxh3 = gui_data.main_notebook.radio_button_hash_type_xxh3.clone();
-    let radio_button_hash_type_crc32 = gui_data.main_notebook.radio_button_hash_type_crc32.clone();
-    radio_button_duplicates_name.connect_toggled(move |_| {
-        if radio_button_duplicates_hash.is_active() {
-            radio_button_hash_type_blake3.set_sensitive(true);
-            radio_button_hash_type_xxh3.set_sensitive(true);
-            radio_button_hash_type_crc32.set_sensitive(true);
-        } else {
-            radio_button_hash_type_blake3.set_sensitive(false);
-            radio_button_hash_type_xxh3.set_sensitive(false);
-            radio_button_hash_type_crc32.set_sensitive(false);
-        }
-    });
-    let radio_button_duplicates_hash = gui_data.main_notebook.radio_button_duplicates_hash.clone();
-    let radio_button_duplicates_size = gui_data.main_notebook.radio_button_duplicates_size.clone();
-    let radio_button_hash_type_blake3 = gui_data.main_notebook.radio_button_hash_type_blake3.clone();
-    let radio_button_hash_type_xxh3 = gui_data.main_notebook.radio_button_hash_type_xxh3.clone();
-    let radio_button_hash_type_crc32 = gui_data.main_notebook.radio_button_hash_type_crc32.clone();
-    radio_button_duplicates_size.connect_toggled(move |_| {
-        if radio_button_duplicates_hash.is_active() {
-            radio_button_hash_type_blake3.set_sensitive(true);
-            radio_button_hash_type_xxh3.set_sensitive(true);
-            radio_button_hash_type_crc32.set_sensitive(true);
-        } else {
-            radio_button_hash_type_blake3.set_sensitive(false);
-            radio_button_hash_type_xxh3.set_sensitive(false);
-            radio_button_hash_type_crc32.set_sensitive(false);
-        }
-    });
 }
