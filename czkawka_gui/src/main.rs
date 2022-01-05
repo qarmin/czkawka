565823316b4eb829ce8abdab2d479bf8bf4ad6cc
Rafał Mikrut <mikrutrafal@protonmail.com>
2022-01-05 23:04:24 +0100

    Adin

--- czkawka_gui/src/main.rs
+++ czkawka_gui/src/main.rs
@@ -1,23 +1,47 @@
 // Remove console window in Windows OS
 #![windows_subsystem = "windows"]
 
-use gtk::prelude::*;
+use i18n_embed::unic_langid::LanguageIdentifier;
+use i18n_embed::DesktopLanguageRequester;
 
 use crate::connect_change_language::*;
 
 mod connect_change_language;
 
 fn main() {
-    let application = gtk::Application::builder().build();
-    application.connect_activate(move |application| {
-        load_system_language(); // Check for default system language, must be loaded after initializing GUI and before loading settings from file
+    load_system_language(); // Check for default system language, must be loaded after initializing GUI and before loading settings from file
+    connect_change_language();
+}
+pub fn connect_change_language() {
+    change_language();
+}
 
-        connect_change_language();
+fn change_language() {
+    println!("Change language");
+    let localizers = vec![("czkawka_gui", czkawka_core::localizer::localizer())];
 
-        let window_main = gtk::Window::builder().application(application).build();
-        window_main.show_all();
-        window_main.connect_delete_event(move |_, _| Inhibit(false));
-    });
+    let lang_identifier = vec![LanguageIdentifier::from_bytes("en".as_bytes()).unwrap()];
+    for (lib, localizer) in localizers {
+        if let Err(error) = localizer.select(&lang_identifier) {
+            eprintln!("Error while loadings languages for {} {:?}", lib, error);
+        }
+    }
+}
 
-    application.run();
+pub fn load_system_language() {
+    println!("Load system language");
+    let requested_languages = DesktopLanguageRequester::requested_languages();
+
+    if let Some(language) = requested_languages.get(0) {
+        let old_short_lang = language.to_string();
+        let mut short_lang = "".to_string();
+        // removes from e.g. en_zb, ending _zd since Czkawka don't support this(maybe could add this in future, but only when)
+        for i in old_short_lang.chars() {
+            if i.is_ascii_alphabetic() {
+                short_lang.push(i)
+            } else {
+                break;
+            }
+        }
+    }
 }
