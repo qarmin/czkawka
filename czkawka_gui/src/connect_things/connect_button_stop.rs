604cca8bbd852e7ab7b70f7d5bc34b5cacfe58d2
KerfuffleV2 <kerfluffle@keemail.me>
2022-02-14 04:46:56 -0700

    Change stop message channel from unbounded to bounded with size 1.

--- czkawka_gui/src/connect_things/connect_button_stop.rs
+++ czkawka_gui/src/connect_things/connect_button_stop.rs
@@ -1,7 +1,15 @@
+use crossbeam_channel::{Sender, TrySendError};
 use gtk::prelude::*;
 
 use crate::gui_structs::gui_data::GuiData;
 use crate::help_functions::KEY_ENTER;
+
+fn send_stop_message(stop_sender: &Sender<()>) {
+    stop_sender
+        .try_send(())
+        .map_or_else(|e| if matches!(e, TrySendError::Full(_)) { Ok(()) } else { Err(e) }, |_| Ok(()))
+        .unwrap();
+}
 
 pub fn connect_button_stop(gui_data: &GuiData) {
     let evk_button_stop_in_dialog = gui_data.progress_window.evk_button_stop_in_dialog.clone();
@@ -9,14 +17,14 @@ pub fn connect_button_stop(gui_data: &GuiData) {
     evk_button_stop_in_dialog.connect_key_released(move |_, _, key_code, _| {
         if key_code == KEY_ENTER {
             // Only accept enter key to stop search
-            stop_sender.send(()).unwrap();
+            send_stop_message(&stop_sender);
         }
     });
 
     let button_stop_in_dialog = gui_data.progress_window.button_stop_in_dialog.clone();
     let stop_sender = gui_data.stop_sender.clone();
     button_stop_in_dialog.connect_button_release_event(move |_, _e| {
-        stop_sender.send(()).unwrap();
+        send_stop_message(&stop_sender);
         gtk::Inhibit(false)
     });
 
