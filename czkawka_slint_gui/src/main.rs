1c044d2ad85ee35af3f23c01d73395425f42d16e
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-10-17 16:30:43 +0200

    A1

--- czkawka_slint_gui/src/main.rs
+++ czkawka_slint_gui/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {}
 
-slint::slint! {}
+slint::slint! {
+    import { Button, VerticalBox , HorizontalBox, TabWidget, ListView, StandardListView, StandardTableView} from "std-widgets.slint";
+    export component MainWindow {
+        VerticalBox {
+            TabWidget {
+                width: 400px;
+                preferred-height: 300px;
+                Tab {
+                    title: "Empty Folders";
+                    ListView {
+
+                    }
+                    StandardTableView {
+                    visible: true;
+                        columns: [{title: "Selection", Sort: false}, {title: "Name"}, {title: "Path"}];
+                        rows: [
+                            [{ text: "FG"}, {text: "gasg"}, {text:"asg" }],
+                            [{ text: "GS"}, {text: "WW"}, {text:"AGAS" }]
+                        ];
+
+                    }
+
+                }
+                Tab {
+                    title: "Empty Files";
+                    Text {
+                        text: "Empty files";
+                    }
+                }
+            }
+            HorizontalBox {
+                Button {
+                    text: "Scan";
+                }
+                Button {
+                    text: "Delete";
+                }
+            }
+        }
+    }
+
+}
