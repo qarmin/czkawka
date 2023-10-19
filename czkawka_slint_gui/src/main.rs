98590adbd1c19e0228f929513c4cbf69b5703531
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-10-19 09:32:37 +0200

    AB

--- czkawka_slint_gui/src/main.rs
+++ czkawka_slint_gui/src/main.rs
@@ -1,7 +1,21 @@
+use std::rc::Rc;
+use slint::{Model, ModelRc, SharedString, StandardListViewItem, VecModel};
 slint::include_modules!();
 
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
-}
+    let app = MainWindow::new().unwrap();//.run().unwrap();
+    let row_data: Rc<VecModel<ModelRc<SharedString>>> = Rc::new(VecModel::default());
 
-slint::slint! {}
+    for r in 0..10000000 {
+        let items = VecModel::default();
+
+        for c in 0..3 {
+            items.push(slint::format!("Item {r}.{c}").into());
+        }
+
+        row_data.push(ModelRc::new(items));
+    }
+    app.set_empty_folder_model(row_data.into());
+
+    app.run().unwrap();
+}
