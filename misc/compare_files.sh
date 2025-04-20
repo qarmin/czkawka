35a23efc45e9ae07072438160e70839dccff5489
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-04-20 07:44:56 +0200

    Clean

--- misc/compare_files.sh
+++ misc/compare_files.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 # Calculate individual MD5 hashes with file names and join them with commas
-MD1=$(md5sum czkawka_cli_debug_1 czkawka_gui_debug_1 krokiet_gui_debug_1 czkawka_cli_release_1 czkawka_gui_release_1 krokiet_gui_release_1  | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
-MD2=$(md5sum czkawka_cli_debug_2 czkawka_gui_debug_2 krokiet_gui_debug_2 czkawka_cli_release_2 czkawka_gui_release_2 krokiet_gui_release_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_DEBUG_1 =$(md5sum czkawka_cli_debug_1 czkawka_gui_debug_1 krokiet_debug_1 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_DEBUG_2 =$(md5sum czkawka_cli_debug_2 czkawka_gui_debug_2 krokiet_debug_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_RELEASE_1=$(md5sum czkawka_cli_release_1 czkawka_gui_release_1 krokiet_release_1 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_RELEASE_2=$(md5sum czkawka_cli_release_2 czkawka_gui_release_2 krokiet_release_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
 
 # Print MD5 hashes
-echo "MD5 1 - $MD1"
-echo "MD5 2 - $MD2"
+echo "MD5_DEBUG_1: $MD5_DEBUG_1"
+echo "MD5_DEBUG_2: $MD5_DEBUG_2"
+echo ""
+echo "MD5_RELEASE_1: $MD5_RELEASE_1"
+echo "MD5_RELEASE_2: $MD5_RELEASE_2"
 
-# Compare and fail if MD1 != MD2
-if [ "$MD1" != "$MD2" ]; then
-  echo "MD5 hashes do not match. Failing."
-  exit 1
+if [ "$MD5_DEBUG_1" == "$MD5_DEBUG_2" ]; then
+    echo "DEBUG files are the same"
+    exit 1
 else
-  echo "MD5 hashes match. Success."
+    echo "DEBUG files are different"
+fi
+if [ "$MD5_RELEASE_1" == "$MD5_RELEASE_2" ]; then
+    echo "RELEASE files are the same"
+    exit 1
+else
+    echo "RELEASE files are different"
 fi
