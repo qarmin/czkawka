2cbc52fac87a0e7ee623cb4324eb1c832af9dff7
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-04-20 08:55:02 +0200

    EXT

--- misc/compare_files.sh
+++ misc/compare_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Calculate individual MD5 hashes with file names and join them with commas
-MD5_DEBUG_1 =$(md5sum czkawka_cli_debug_1 czkawka_gui_debug_1 krokiet_debug_1 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
-MD5_DEBUG_2 =$(md5sum czkawka_cli_debug_2 czkawka_gui_debug_2 krokiet_debug_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_DEBUG_1=$(md5sum czkawka_cli_debug_1 czkawka_gui_debug_1 krokiet_debug_1 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
+MD5_DEBUG_2=$(md5sum czkawka_cli_debug_2 czkawka_gui_debug_2 krokiet_debug_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
 MD5_RELEASE_1=$(md5sum czkawka_cli_release_1 czkawka_gui_release_1 krokiet_release_1 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
 MD5_RELEASE_2=$(md5sum czkawka_cli_release_2 czkawka_gui_release_2 krokiet_release_2 | awk '{print $1 "(" $2 ")"}' | paste -sd ",")
 
