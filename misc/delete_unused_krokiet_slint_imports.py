d2d240fe31ab663d33cb0c671db3299ea6c77513
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-05-06 22:52:49 +0200

    DJ

--- misc/delete_unused_krokiet_slint_imports.py
+++ misc/delete_unused_krokiet_slint_imports.py
@@ -4,7 +4,6 @@ import re
 script_path = os.path.dirname(os.path.abspath(__file__))
 ui_path = f"{script_path}/../krokiet/ui"
 
-# Collect all .slint files in the ui_path directory
 collected_files = [
     os.path.join(root, file)
     for root, _, files in os.walk(ui_path)
@@ -20,36 +19,39 @@ for file_path in collected_files:
     imports_to_check = []
     updated_lines = []
 
-    # Separate imports and other lines
     for line in lines:
         if line.startswith("import"):
             imports_to_check.append(line)
         else:
+            if len(non_import_lines) == 0 and len(line.strip()) == 0:
+                continue
             non_import_lines.append(line)
 
-    # Check usage of each import
+    non_imported_content = "\n".join(non_import_lines)
+
     for import_line in imports_to_check:
-        imported_items = re.search(r"\{(.*?)\}", import_line)
+        imported_items = [i.strip() for i in import_line.split("{")[1].split("}")[0].split(",") if len(i.strip()) > 0]
         if not imported_items:
             continue
 
-        imported_items = [item.strip() for item in imported_items.group(1).split(",")]
         from_file = import_line.split("from")[1].strip()
 
-        # Check if each imported item is used in the file
         used_items = []
         for item in imported_items:
             regex = rf"\b{item}\b"
-            if len(re.findall(regex, "\n".join(non_import_lines))) >= 2:
+            if len(re.findall(regex, non_imported_content)) > 1:
                 used_items.append(item)
 
-        # Reassemble the import line with only used items
         if used_items:
-            updated_lines.append(f"import {{ {', '.join(used_items)} }} from {from_file};")
+            updated_line = f"import {{ {', '.join(used_items)} }} from {from_file}"
+            updated_line = updated_line.replace(";;", ";")
+            updated_lines.append(updated_line)
 
-    # Add non-import lines back
+    if len(updated_lines) != 0:
+        updated_lines.append("")
+
     updated_lines.extend(non_import_lines)
+    updated_lines.append("\n")
 
-    # Write the updated content back to the file
     with open(file_path, "w", encoding="utf-8") as file:
         file.write("\n".join(updated_lines))
