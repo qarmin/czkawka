cd37603b8fd75e25afd2e7a4336c8b2af000c8f8
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-08-08 22:56:43 +0200

    Misc

---
+++ misc/test_compilation_speed_size/src/main.rs
@@ -0,0 +1,213 @@
+mod model;
+
+use std::fs;
+use std::fs::OpenOptions;
+use std::path::Path;
+
+use walkdir::WalkDir;
+
+use crate::model::{BuildOrCheck, CodegenUnits, Config, Incremental, LTO, OptLevel, OverflowChecks, Panic, Results, SplitDebug, Debugg};
+
+const START_CONFIG_TOML: &str = r#"[workspace]
+members = [
+    "czkawka_core",
+    "czkawka_cli",
+    "czkawka_gui",
+    "krokiet"
+]
+exclude = [
+    "misc/test_read_perf",
+    "misc/test_image_perf",
+    "misc/test_compilation_speed_size",
+    "ci_tester",
+]
+resolver = "3""#;
+
+fn main() {
+    let first_arg = std::env::args().nth(1).expect("Please provide the Czkawka root path");
+    let config_toml_path = Path::new(&first_arg).join("Cargo.toml");
+    let config_toml_content = fs::read_to_string(&config_toml_path).expect(format!("Could not read config.toml file at {}", config_toml_path.display()).as_str());
+
+    if !config_toml_content.starts_with(START_CONFIG_TOML) {
+        panic!("The config.toml file does not start with the expected content. Please use czkawka repo.");
+    }
+
+    let mut new_content_base = START_CONFIG_TOML.to_string();
+    new_content_base.push_str("\n\n\n[profile.fff]\ninherits=\"dev\"");
+
+    let mut results_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(Path::new(&first_arg).join("compilation_results.txt"))
+        .expect("Could not open results file");
+
+    Results::write_header_to_file(&mut results_file).unwrap();
+
+    for project in ["krokiet"] { //, "krokiet"] {
+        for threads_number in [24, 4] {
+            for config in get_configs() {
+                let new_content = format!("{new_content_base}\n{}\n", config.to_str());
+                fs::write(&config_toml_path, new_content).expect("Could not write config file");
+
+                let result = check_compilation_speed_and_size(&first_arg, project, config, threads_number);
+                result.save_to_file(&mut results_file).expect("Could not save results to file");
+            }
+        }
+    }
+}
+
+fn get_configs() -> Vec<Config> {
+    //[profile.dev]
+    // opt-level = 0
+    // debug = true
+    // split-debuginfo = '...'  # Platform-specific.
+    // strip = "none"
+    // debug-assertions = true
+    // overflow-checks = true
+    // lto = false
+    // panic = 'unwind'
+    // incremental = true
+    // codegen-units = 256
+    // rpath = false
+
+    // [profile.release]
+    // opt-level = 3
+    // debug = false
+    // split-debuginfo = '...'  # Platform-specific.
+    // strip = "none"
+    // debug-assertions = false
+    // overflow-checks = false
+    // lto = false
+    // panic = 'unwind'
+    // incremental = false
+    // codegen-units = 16
+    // rpath = false
+    let debug_base = Config {
+        name: "debug",
+        lto: LTO::Off,
+        debug: Debugg::Full,
+        opt_level: OptLevel::Zero,
+        build_or_check: BuildOrCheck::Build,
+        codegen_units: CodegenUnits::Default,
+        panic: Panic::Unwind,
+        split_debug: SplitDebug::Off,
+        overflow_checks: OverflowChecks::On,
+        incremental: Incremental::On,
+    };
+    let release_base = Config {
+        name: "release",
+        lto: LTO::Off,
+        debug: Debugg::None,
+        opt_level: OptLevel::Three,
+        build_or_check: BuildOrCheck::Build,
+        codegen_units: CodegenUnits::Sixteen,
+        panic: Panic::Unwind,
+        split_debug: SplitDebug::Off,
+        overflow_checks: OverflowChecks::Off,
+        incremental: Incremental::Off,
+    };
+
+    let mut debug_fast_check = debug_base.clone();
+    debug_fast_check.name = "debug_fast_check";
+    debug_fast_check.debug = Debugg::None;
+
+    let mut check_fast_check = debug_fast_check.clone();
+    check_fast_check.name = "check_fast_check";
+    check_fast_check.build_or_check = BuildOrCheck::Check;
+
+    let mut release_thin_lto = release_base.clone();
+    release_thin_lto.name = "release_thin_lto";
+    release_thin_lto.lto = LTO::Thin;
+
+    let mut release_full_lto = release_base.clone();
+    release_full_lto.name = "release_fat_lto";
+    release_full_lto.lto = LTO::Fat;
+
+    vec![debug_base, release_base, release_thin_lto, release_full_lto, debug_fast_check, check_fast_check]
+}
+
+fn clean_cargo() {
+    println!("Cleaning cargo...");
+    let output = std::process::Command::new("cargo").arg("clean")
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit()).output().expect("Failed to execute cargo clean");
+
+    if !output.status.success() {
+        panic!("Cargo clean failed: {}", String::from_utf8_lossy(&output.stderr));
+    }
+}
+
+fn run_cargo_build(project: &str, threads_number: u32, build: BuildOrCheck) {
+    let build_check = if build == BuildOrCheck::Build {
+        "build"
+    } else {
+        "check"
+    };
+    let mut command = std::process::Command::new("cargo");
+    command
+        .env("CARGO_BUILD_JOBS", threads_number.to_string())
+        .arg(build_check)
+        .arg("--package")
+        .arg(project)
+        .arg("--profile")
+        .arg("fff")
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+    ;
+
+    let output = command.output().expect("Failed to execute cargo build");
+
+    if !output.status.success() {
+        panic!("Cargo build failed: {}", String::from_utf8_lossy(&output.stderr));
+    }
+}
+
+fn check_compilation_speed_and_size(base: &str, project: &str, config: Config, threads_number: u32) -> Results {
+    clean_cargo();
+
+    let start_time = std::time::Instant::now();
+
+    println!("Running cargo build for project: {}", project);
+
+    println!("Project: {project}, threads: {threads_number}, {}", config.to_string_short());
+
+    run_cargo_build(project, threads_number, config.build_or_check);
+
+    let compilation_time = start_time.elapsed();
+
+    let output_file_size = get_size_of_output_file(base, project);
+    let target_folder_size = get_size_of_target_folder(base);
+
+    Results {
+        output_file_size,
+        target_folder_size,
+        compilation_time,
+        config,
+        threads_number,
+        project: project.to_string(),
+    }
+}
+
+fn get_size_of_output_file(base: &str, project: &str) -> u64 {
+    let output_path = Path::new(base).join("target").join("fff").join(project);
+    if output_path.exists() {
+        output_path.metadata().map(|e| e.len()).unwrap_or_default()
+    } else {
+        0
+    }
+}
+
+fn get_size_of_target_folder(base: &str) -> u64 {
+    let target_path = Path::new(base).join("target");
+    get_size_of_files_in_folder(&target_path)
+}
+
+fn get_size_of_files_in_folder(folder: &Path) -> u64 {
+    WalkDir::new(folder)
+        .max_depth(999)
+        .into_iter()
+        .flatten()
+        .map(|e| e.metadata().map(|e| e.len()).unwrap_or_default())
+        .sum()
+}
---
+++ misc/test_compilation_speed_size/src/model.rs
@@ -0,0 +1,232 @@
+use std::io::Write;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Debugg {
+    None,
+    LineDirectivesOnly,
+    LineTablesOnly,
+    Limited,
+    Full,
+}
+
+impl Debugg {
+    fn to_str(&self) -> &str {
+        match self {
+            Debugg::None => "debug=\"none\"",
+            Debugg::LineDirectivesOnly => "debug=\"line-directives-only\"",
+            Debugg::LineTablesOnly => "debug=\"line-tables-only\"",
+            Debugg::Limited => "debug=1",
+            Debugg::Full => "debug=2",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum SplitDebug {
+    Off,
+    Packed,
+    Unpacked,
+}
+impl SplitDebug {
+    fn to_str(&self) -> &str {
+        match self {
+            SplitDebug::Off => "split-debuginfo=\"off\"",
+            SplitDebug::Packed => "split-debuginfo=\"packed\"",
+            SplitDebug::Unpacked => "split-debuginfo=\"unpacked\"",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum OptLevel {
+    Zero,
+    One,
+    Two,
+    Three,
+    S,
+}
+impl OptLevel {
+    fn to_str(&self) -> &str {
+        match self {
+            OptLevel::Zero => "opt-level=0",
+            OptLevel::One => "opt-level=1",
+            OptLevel::Two => "opt-level=2",
+            OptLevel::Three => "opt-level=3",
+            OptLevel::S => "opt-level=s",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum LTO {
+    Off,
+    Thin,
+    Fat,
+}
+
+impl LTO {
+    fn to_str(&self) -> &str {
+        match self {
+            LTO::Off => "lto=\"off\"",
+            LTO::Thin => "lto=\"thin\"",
+            LTO::Fat => "lto=\"fat\"",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BuildOrCheck {
+    Build,
+    Check,
+}
+
+impl BuildOrCheck {
+    fn to_str(&self) -> &str {
+        match self {
+            BuildOrCheck::Build => "build",
+            BuildOrCheck::Check => "check",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum CodegenUnits {
+    One,
+    Sixteen,
+    Default,
+}
+impl CodegenUnits {
+    fn to_str(&self) -> &str {
+        match self {
+            CodegenUnits::One => "codegen-units=1",
+            CodegenUnits::Sixteen => "codegen-units=16",
+            CodegenUnits::Default => "codegen-units=256",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Panic {
+    Unwind,
+    Abort,
+}
+impl Panic {
+    fn to_str(&self) -> &str {
+        match self {
+            Panic::Unwind => "panic=\"unwind\"",
+            Panic::Abort => "panic=\"abort\"",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum OverflowChecks {
+    On,
+    Off,
+}
+impl OverflowChecks {
+    fn to_str(&self) -> &str {
+        match self {
+            OverflowChecks::On => "overflow-checks=true",
+            OverflowChecks::Off => "overflow-checks=false",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Incremental {
+    On,
+    Off,
+}
+impl Incremental {
+    fn to_str(&self) -> &str {
+        match self {
+            Incremental::On => "incremental=true",
+            Incremental::Off => "incremental=false",
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub name: &'static str,
+    pub lto: LTO,
+    pub debug: Debugg,
+    pub opt_level: OptLevel,
+    pub build_or_check: BuildOrCheck,
+    pub codegen_units: CodegenUnits,
+    pub panic: Panic,
+    pub split_debug: SplitDebug,
+    pub overflow_checks: OverflowChecks,
+    pub incremental: Incremental,
+}
+
+impl Config {
+    pub fn to_str(&self) -> String {
+        // format!(
+        //     "{}\n{}\n{}\n",
+        //     self.lto.to_str(),
+        //     self.debug.to_str(),
+        //     self.opt_level.to_str(),
+        // )
+        // All the fields with to_str split by \n, but without name
+        format!(
+            "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
+            self.lto.to_str(),
+            self.debug.to_str(),
+            self.opt_level.to_str(),
+            self.codegen_units.to_str(),
+            self.panic.to_str(),
+            self.split_debug.to_str(),
+            self.overflow_checks.to_str(),
+            self.incremental.to_str()
+        )
+    }
+    pub fn to_string_short(&self) -> String {
+        // All the fields with to_str split by space, but without name
+        format!(
+            "LTO: {}, Debug: {}, Opt: {}, Build/Check: {}, Codegen Units: {}, Panic: {}, Split Debug: {}, Overflow Checks: {}, Incremental: {}",
+            self.lto.to_str(),
+            self.debug.to_str(),
+            self.opt_level.to_str(),
+            self.build_or_check.to_str(),
+            self.codegen_units.to_str(),
+            self.panic.to_str(),
+            self.split_debug.to_str(),
+            self.overflow_checks.to_str(),
+            self.incremental.to_str()
+        )
+    }
+}
+
+pub struct Results {
+    pub output_file_size: u64,
+    pub target_folder_size: u64,
+    pub compilation_time: std::time::Duration,
+    pub config: Config,
+    pub threads_number: u32,
+    pub project: String,
+}
+
+impl Results {
+    pub fn write_header_to_file(file_writer: &mut std::fs::File) -> std::io::Result<()> {
+        writeln!(
+            file_writer,
+            "Config | Output File Size (bytes) | Target Folder Size (bytes) | Compilation Time (seconds) | Threads"
+        )?;
+        Ok(())
+    }
+    pub fn save_to_file(&self, file_writer: &mut std::fs::File) -> std::io::Result<()> {
+        writeln!(
+            file_writer,
+            "{}_{} | {} | {} | {} | {}",
+            self.config.name,
+            self.project,
+            self.output_file_size,
+            self.target_folder_size,
+            self.compilation_time.as_secs(),
+            self.threads_number
+        )?;
+        Ok(())
+    }
+}
