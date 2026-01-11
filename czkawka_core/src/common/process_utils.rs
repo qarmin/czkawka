44e3ecca0ce4497f34d28c15e5a757644b7abef6
Rafał Mikrut <mikrutrafal@protonmail.com>
2026-01-11 16:23:44 +0100

    Soc

--- czkawka_core/src/common/process_utils.rs
+++ czkawka_core/src/common/process_utils.rs
@@ -6,57 +6,13 @@ use std::time::{Duration, Instant};
 
 use log::warn;
 
-// Returns None if stopped, Some(Err) if command failed, Some(Ok(output)) if successful.
-pub fn run_command_interruptible2(mut command: Command, stop_flag: &Arc<AtomicBool>) -> Option<Result<std::process::Output, String>> {
-    if stop_flag.load(Ordering::Relaxed) {
-        return None;
-    }
-
-    command.stdin(Stdio::null()).stdout(Stdio::piped()).stderr(Stdio::piped());
-    // command.stdin(Stdio::null());
-
-    let mut child = match command.spawn() {
-        Ok(c) => c,
-        Err(e) => return Some(Err(format!("Failed to spawn command: {e}"))),
-    };
-
-    let stop_flag = stop_flag.clone();
-    let start_time = Instant::now();
-    let warning_steps = [50, 250, 1250, 6000];
-    let mut next_warning_idx = 0;
-
-    loop {
-        if stop_flag.load(Ordering::Relaxed) {
-            let _ = child.kill();
-            return None;
-        }
-
-        let elapsed_secs = start_time.elapsed().as_secs();
-        if let Some(warning_time) = warning_steps.get(next_warning_idx)
-            && elapsed_secs >= *warning_time
-        {
-            warn!("Command is still running after {warning_time} seconds, for command: {command:?}");
-            next_warning_idx += 1;
-        }
-
-        match child.try_wait() {
-            Ok(Some(_status)) => {
-                return match child.wait_with_output() {
-                    Ok(output) => Some(Ok(output)),
-                    Err(e) => Some(Err(format!("Failed to get output: {e}"))),
-                };
-            }
-            Ok(None) => {
-                thread::sleep(Duration::from_millis(100));
-            }
-            Err(e) => {
-                return Some(Err(format!("Failed to check process status: {e}")));
-            }
-        }
-    }
+pub struct CommandOutput {
+    pub status: std::process::ExitStatus,
+    pub stdout: String,
+    pub stderr: String,
 }
 
-pub fn run_command_interruptible(mut command: Command, stop_flag: &Arc<AtomicBool>) -> Option<Result<std::process::Output, String>> {
+pub fn run_command_interruptible(mut command: Command, stop_flag: &Arc<AtomicBool>) -> Option<Result<CommandOutput, String>> {
     if stop_flag.load(Ordering::Relaxed) {
         return None;
     }
@@ -130,5 +86,5 @@ pub fn run_command_interruptible(mut command: Command, stop_flag: &Arc<AtomicBoo
     let stdout = Arc::try_unwrap(stdout_buf).unwrap().into_inner().unwrap();
     let stderr = Arc::try_unwrap(stderr_buf).unwrap().into_inner().unwrap();
 
-    Some(Ok(std::process::Output { status, stdout, stderr }))
+    Some(Ok(CommandOutput { status, stdout: String::from_utf8_lossy(&stdout).to_string(), stderr: String::from_utf8_lossy(&stderr).to_string() }))
 }
