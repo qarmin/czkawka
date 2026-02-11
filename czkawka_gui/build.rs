26d74f500d8319543f312ad41243092eeec03b09
Rafał Mikrut <mikrutrafal@protonmail.com>
2026-02-11 19:35:29 +0100

    EXE

--- czkawka_gui/build.rs
+++ czkawka_gui/build.rs
@@ -1,12 +1,6 @@
 use std::env;
 
 fn main() {
-    if env::var("SLINT_STYLE").is_err() || env::var("SLINT_STYLE") == Ok(String::new()) {
-        slint_build::compile_with_config("ui/main_window.slint", slint_build::CompilerConfiguration::new().with_style("fluent-dark".into())).expect("Unable to compile slint file");
-    } else {
-        slint_build::compile("ui/main_window.slint").expect("Unable to compile slint file");
-    }
-
     if env::var("CARGO_FEATURE_ADD_WINDOWS_ICON").is_ok() {
         if let Ok(target_os) = env::var("CARGO_CFG_TARGET_OS") {
             if target_os == "windows" {
